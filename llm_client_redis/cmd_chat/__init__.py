ef83f10d29c7ca1ee2e739725466a49a7958b0c9
fjw12998 <fjw12998@hotmail.com>
2025-07-24 17:25:40 +0800

    refactor(project): 重构项目目录并优化代码
    - 将项目根目录从 llm_client_redis改为 src/llm_client_redis
    - 优化代码结构，提高代码可读性和可维护性- 更新项目配置文件和文档

--- llm_client_redis/cmd_chat/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    'ChatSession',
-    'AbstractChatSession',
-    'Command',
-    'CommandRegistry'
-]
-
-from .chat_session import ChatSession
-from .command_def import AbstractChatSession, Command, CommandRegistry
--- llm_client_redis/cmd_chat/cmd_templates/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    'SendTemplateCommand',
-    'ShowTextBlockCommand',
-    'TextBlockCommand'
-           ]
-
-from .send_template_cmd import SendTemplateCommand
-from .show_text_block_cmd import ShowTextBlockCommand
-from .text_block_command import TextBlockCommand
