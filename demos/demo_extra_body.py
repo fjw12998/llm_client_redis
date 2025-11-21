774d0218027ffe003630bbcd22af8857d49a64da
fjw12998 <fjw12998@hotmail.com>
2025-11-21 17:02:11 +0800

    ``` docs(demos): 添加模型思考模式与续写功能测试说明
    为 `test_request_stream_v31` 和 `test_request_stream_v3` 方法添加详细注释，说明
    DeepSeek 系列模型对思考模式的支持情况及续写功能测试目的。
    参考华为云文档明确了不同版本模型的默认行为和参数支持范围。
    ```

--- demos/demo_extra_body.py
+++ demos/demo_extra_body.py
@@ -23,6 +23,21 @@ def test_request(llm_client_redis: LLMClientRedis):
 
 
 def test_request_stream_v31(llm_client_redis: LLMClientRedis):
+    """
+    资料: https://support.huaweicloud.com/usermanual-maas-modelarts/maas-modelarts-0083.html
+    我查了上面的资料, DeepSeek-R1 与 DeepSeek-R1-0528 不支持关闭思考模式; DeepSeek-V3.1 与 DeepSeek-V3.2 默认关闭思考模式, 但可以打开
+    
+    模型名称    thinking.type默认值    thinking.type支持的取值
+    
+    DeepSeek-R1    enabled    enabled
+    
+    DeepSeek-R1-0528    enabled    enabled
+    
+    DeepSeek-V3.1    disabled    enabled/disabled
+    
+    DeepSeek-V3.2    disabled    enabled/disabled
+
+    """
     model: str = "huawei_deepseek_v3.1"
     messages: List[BaseMessage] = [HumanMessage("写个100字的故事"), AIMessage("不好")]
     
@@ -39,6 +54,9 @@ def test_request_stream_v31(llm_client_redis: LLMClientRedis):
     logging.info(result)
 
 def test_request_stream_v3(llm_client_redis: LLMClientRedis):
+    """
+    测试 deepseek-v3 是否能实现继写，即支持 continue_final_message 与 add_generation_prompt 参数
+    """
     model: str = "huawei_deepseek_v3_64k"
     messages: List[BaseMessage] = [HumanMessage("写个100字的故事"), AIMessage("不好")]
     
